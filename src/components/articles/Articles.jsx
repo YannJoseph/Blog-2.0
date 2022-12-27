1ad6e94b3dae455423b9956c3b9b61af20c3f8d0
Yann <yann_jo@yahoo.fr>
2022-12-27 16:10:50 +0100

    add axios for catch data

--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+// import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
+
+
+
+
+    
 function Articles() {
+
+    const[ data, setData]= useState([])
+
+
+
+    // axios.get('http://localhost:8001/blog')
+    // .then(function(data){
+    //     console.log(data);
+    // })
+    // .catch((function(error){
+    //     console.log(error);
+    // })
+    // .then(function(){
+
+    // }));
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(
+                'https://jsonplaceholder.typicode.com/posts'
+            );
+            setData(result.data)
+        };
+        fetchData()
+    })
+    
   return (
-    <div>Articles
-    <h1>hello world</h1>
+    <div>
+        <ul>{data.map(item => (
+            <li key={item.id}>
+                <p>{item.title}</p>
+            </li>
+        ))}
+    </ul>
+        
+   
     </div>
   )
 }
